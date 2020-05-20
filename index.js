d8c8f353584dfb229b84edf6742f7dcc952b060e
Shilldon <stewart.s-hill@ntlworld.com>
2020-05-20 21:28:24 +0100

    amended index.js to bare bones to test deployment including authentication

--- index.js
+++ index.js
@@ -103,14 +103,14 @@ app.delete('/logout', (req,res) => {
     req.logOut();
     res.redirect('/login');
 })
-
+*/
 function checkAuthenticated(req, res, next) {
     if(req.isAuthenticated()) {
         return next()
     }
     res.redirect('/login')
 }
-
+/*
 function checkNotAuthenticated(req, res, next) {
     if(req.isAuthenticated()) {
         return res.redirect('/') 
